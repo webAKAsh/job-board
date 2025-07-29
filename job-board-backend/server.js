ff06b5869471ded1ca83d00974cc58da17c864ff
webAKAsh <akash.garg@anantla.com>
2025-07-29 22:22:06 +0530

    bug server.js

--- job-board-backend/server.js
+++ job-board-backend/server.js
@@ -19,17 +19,14 @@ mongoose
   .catch((err) => console.error(err));
 
 // Job Schema
-const jobSchema = new mongoose.Schema(
-  {
-    title: { type: String, required: true },
-    company: { type: String, required: true },
-    type: { type: String, required: true },
-    location: { type: String, required: true },
-    description: { type: String, required: true },
-    createdAt: { type: Date, default: Date.now },
-  },
-  { collection: "jobs" }
-);
+const jobSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  company: { type: String, required: true },
+  type: { type: String, required: true },
+  location: { type: String, required: true },
+  description: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
+});
 
 const Job = mongoose.model("Job", jobSchema);
 
